921a9eeb63249cf5864f77b353f107aeea8ee953
Sergei <seregagleb1@yandex.ru>
2023-12-15 01:04:26 +0300

    refactor:Сделать TaskManager интерфейсом refactor:Создать InMemoryTaskManager, который реализует методы TaskManager

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import Manager.InMemoryTaskManager;
+import Manager.TaskManager;
 import Tasks.Epic;
 import Tasks.Status;
 import Tasks.Subtask;
@@ -6,80 +8,80 @@ import Tasks.Task;
  * Все проверки, которые были указаны в ТЗ прошли проверку без ошибок.*/
 public class Main {
     public static void main(String[] args) {
-        Manager manager = new Manager();
+        InMemoryTaskManager inMemoryTaskManager = new InMemoryTaskManager();
         Task task;
         Epic epic;
         Subtask subtask;
 
         task = new Task("First task", "To do smth", Status.NEW);
-        manager.createTask(task);
+        inMemoryTaskManager.createTask(task);
         task = new Task("Second task", "Smth is Done", Status.NEW);
-        manager.createTask(task);
+        inMemoryTaskManager.createTask(task);
 
         epic = new Epic("First epic", "Epic with 2 subtasks");
-        manager.createEpic(epic);
+        inMemoryTaskManager.createEpic(epic);
         epic = new Epic("Second epic", "Epic with 1 subtask");
-        manager.createEpic(epic);
+        inMemoryTaskManager.createEpic(epic);
 
         subtask = new Subtask("First subtask", "Subtask1 for first epic", Status.NEW, 2);
-        manager.createSubtask(subtask);
+        inMemoryTaskManager.createSubtask(subtask);
         subtask = new Subtask("Second subtask", "Subtask2 for first epic", Status.NEW, 2);
-        manager.createSubtask(subtask);
+        inMemoryTaskManager.createSubtask(subtask);
 
         subtask = new Subtask("First subtask", "Subtask for second epic", Status.NEW, 3);
-        manager.createSubtask(subtask);
+        inMemoryTaskManager.createSubtask(subtask);
 
-        System.out.println(manager.getEpics());
+        System.out.println(inMemoryTaskManager.getEpics());
         System.out.println();
-        System.out.println(manager.getTasks());
+        System.out.println(inMemoryTaskManager.getTasks());
         System.out.println();
-        System.out.println(manager.getSubtasks());
+        System.out.println(inMemoryTaskManager.getSubtasks());
         System.out.println();
 
-        task = manager.getTaskById(0);
+        task = inMemoryTaskManager.getTaskById(0);
         task.setName("Renamed Task1");
         task.setTaskStatus(Status.IN_PROGRESS);
-        manager.updateTask(task, 0);
+        inMemoryTaskManager.updateTask(task, 0);
 
         task = new Task("Second Task", "Redescription task 2", Status.DONE);
         task.setId(1);
-        manager.updateTask(task, 1);
+        inMemoryTaskManager.updateTask(task, 1);
 
-        System.out.println(manager.getTasks());
+        System.out.println(inMemoryTaskManager.getTasks());
         System.out.println();
 
-        epic = manager.getEpicById(2);
+        epic = inMemoryTaskManager.getEpicById(2);
         epic.setName("Renamed epic1");
-        manager.updateEpic(epic, 2);
+        inMemoryTaskManager.updateEpic(epic, 2);
 
         epic = new Epic("Second epic", "Redescription");
         epic.setId(3);
-        epic.setSubtasksList(manager.getAllSubtasksFromEpic(3));
-        manager.updateEpic(epic, 3);
+        epic.setSubtasksList(inMemoryTaskManager.getAllSubtasksFromEpic(3));
+        inMemoryTaskManager.updateEpic(epic, 3);
 
-        System.out.println(manager.getEpics());
+        System.out.println(inMemoryTaskManager.getEpics());
         System.out.println();
 
-        subtask = manager.getSubtaskById(4);
+        subtask = inMemoryTaskManager.getSubtaskById(4);
         subtask.setName("Renamed first subtask of first epic");
         subtask.setSubtaskStatus(Status.IN_PROGRESS);
-        manager.updateSubtask(subtask, 4);
+        inMemoryTaskManager.updateSubtask(subtask, 4);
 
         subtask = new Subtask("First subtask", "Redescription", Status.DONE, 3);
-        manager.updateSubtask(subtask, 6);
+        inMemoryTaskManager.updateSubtask(subtask, 6);
 
-        System.out.println(manager.getSubtasks());
+        System.out.println(inMemoryTaskManager.getSubtasks());
         System.out.println();
 
-        System.out.println(manager.getEpics());
+        System.out.println(inMemoryTaskManager.getEpics());
         System.out.println();
 
-        manager.deleteTaskById(0);
-        manager.deleteEpicById(3);
-        manager.deleteSubtaskById(4);
+        inMemoryTaskManager.deleteTaskById(0);
+        inMemoryTaskManager.deleteEpicById(3);
+        inMemoryTaskManager.deleteSubtaskById(4);
 
-        System.out.println(manager.getTasks());
-        System.out.println(manager.getEpics());
-        System.out.println(manager.getSubtasks());
+        System.out.println(inMemoryTaskManager.getTasks());
+        System.out.println(inMemoryTaskManager.getEpics());
+        System.out.println(inMemoryTaskManager.getSubtasks());
     }
 }
---
+++ src/Manager/TaskManager.java
@@ -0,0 +1,61 @@
+package Manager;
+
+import ManagerBusinessLogic.*;
+import Tasks.Epic;
+import Tasks.Subtask;
+import Tasks.Task;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+/**
+ * Класс управляет всем взаимодействием с Task, Epic и Subtask.
+ * При инициализации создает один Список и три HashMap.
+ * Каждая HashMap хранит в себе объекты соответствующего типа.
+ * Список содержит в себе все HashMap под определенными индексами.
+ * Индексы используются в качестве параметров передаваемыми в методы по типу getTasks -
+ * метод получает индекс нужной HashMap и достает объекты конкретно из нее, не затрагивая
+ * остальные HashMap.
+ */
+
+public interface TaskManager {
+
+    ArrayList getTasks();
+
+    ArrayList getEpics();
+
+    ArrayList getSubtasks();
+
+    void createTask(Task task);
+
+    void createEpic(Epic epic);
+
+    void createSubtask(Subtask subtask);
+
+    void deleteTasks();
+
+    void deleteEpics();
+
+    void deleteSubtasks();
+
+    Task getTaskById(int id);
+
+    Epic getEpicById(int id);
+
+    Subtask getSubtaskById(int id);
+
+    void updateTask(Task task, int id);
+
+    void updateEpic(Epic epic, int id);
+
+    void updateSubtask(Subtask subtask, int id);
+
+    void deleteTaskById(int id);
+
+    void deleteEpicById(int id);
+
+    void deleteSubtaskById(int id);
+
+    ArrayList getAllSubtasksFromEpic(int id);
+}
+
