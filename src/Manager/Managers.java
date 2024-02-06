1831b92118da8b0fe4cb5ac6f3da98103fd17559
Sir Gay <serega112015@gmail.com>
2024-02-06 19:00:50 +0300

    refactor: добавить метод для инициализации FileBuckedTaskManager

--- src/Manager/Managers.java
+++ src/Manager/Managers.java
@@ -1,5 +1,7 @@
 package Manager;
 
+import java.io.File;
+
 /**
  * Класс создает экземпляры менеджеров и занимается передачей ссылок на эти экземпляры.
  */
@@ -9,9 +11,14 @@ public class Managers {
         return new InMemoryTaskManager(getDefaultHistory());
     }
 
+    public static TaskManager getDefaultFileBucked(File file) {
+        return new FileBackedTaskManager(getDefaultHistory(), file);
+    }
+
     public static HistoryManager getDefaultHistory() {
         HistoryManager inMemoryHistoryManager = new InMemoryHistoryManager();
         return inMemoryHistoryManager;
     }
+
 
 }
