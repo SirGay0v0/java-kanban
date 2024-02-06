0495da714b2c69d8bafb29a5f054d8f543232103
Sir Gay <serega112015@gmail.com>
2024-02-06 21:34:26 +0300

    refactor:остановить программу если произошла ошибка ManagerSaveException

--- src/Manager/ManagerSaveException.java
+++ src/Manager/ManagerSaveException.java
@@ -1,7 +1,8 @@
 package Manager;
 
-public class ManagerSaveException extends Exception {
+public class ManagerSaveException extends RuntimeException {
     public ManagerSaveException(){
         System.out.println("Произошла ошибка сохранения файла.");
+        System.exit(0);
     }
 }
